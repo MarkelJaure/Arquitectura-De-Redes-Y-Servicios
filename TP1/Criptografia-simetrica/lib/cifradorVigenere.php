4834af8f4cf64a07de44aff1e347fc8065f85a26
JMarkel <markeljaureguibehere@hotmail.com.ar>
2022-03-19 21:29:03 -0300

    comentados echos en Vigenere

--- TP1/Criptografia-simetrica/lib/cifradorVigenere.php
+++ TP1/Criptografia-simetrica/lib/cifradorVigenere.php
@@ -24,14 +24,14 @@ function cifrarPalabra($aPalabra, $theKeys, $isCifrarActivated)
 
     $newPalabra = str_split($aPalabra);
     for ($i = 0; $i < count($newPalabra); $i++) {
-        echo $newPalabra[$i];
-        echo " -> ";
-        echo $theKeys[$theCount % count($theKeys)];
-        echo " -> ";
+        // echo $newPalabra[$i];
+        // echo " -> ";
+        // echo $theKeys[$theCount % count($theKeys)];
+        // echo " -> ";
         $newPalabra[$i] = desplazarLetra($aPalabra[$i], $theKeys[$theCount % count($theKeys)], $isCifrarActivated);
         $theCount += 1;
-        echo $newPalabra[$i];
-        echo "<br>";
+        // echo $newPalabra[$i];
+        // echo "<br>";
     }
 
     return join("", $newPalabra);
@@ -47,9 +47,9 @@ function changePalabraToArrayOfKeys($aPalabra)
         array_push($someKeys, array_search($aLetra, $arrayAbecedario));
     }
 
-    foreach ($someKeys as $Key) {
-        echo $Key . ", ";
-    }
-    echo "<br>";
+    // foreach ($someKeys as $Key) {
+    //     echo $Key . ", ";
+    // }
+    // echo "<br>";
     return $someKeys;
 }
