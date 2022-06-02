7069522821cd5de0911ad6bdd76a5b50e576c2e9
JMarkel <markeljaureguibehere@hotmail.com.ar>
2022-06-02 16:47:30 -0300

    Tutorial seguido de JWT

--- TP7/toptal-rest-series-toptal-article-02/app.ts
+++ TP7/toptal-rest-series-toptal-article-02/app.ts
@@ -13,10 +13,7 @@ import { UsersRoutes } from "./users/users.routes.config";
 const dotenvResult = dotenv.config();
 if (dotenvResult.error) {
   throw dotenvResult.error;
-} else {
-  console.log(dotenvResult);
 }
-
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
